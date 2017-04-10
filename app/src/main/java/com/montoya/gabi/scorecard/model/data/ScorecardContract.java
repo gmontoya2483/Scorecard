389d32d21830f1ed16f2447971478442a47401cf
Gabriel Montoya <gabi.montoya@gmail.com>
2017-04-10 22:24:18 +0200

    Feat: Task 2 - Content provider
    It was completed the Contract for the GolfFieldEntry and the GolfFieldHoleEntry

--- app/src/main/java/com/montoya/gabi/scorecard/model/data/ScorecardContract.java
+++ app/src/main/java/com/montoya/gabi/scorecard/model/data/ScorecardContract.java
@@ -14,8 +14,15 @@ public class ScorecardContract {
     //Content provider fields
     public static final String CONTENT_AUTHORITY = "com.montoya.gabi.scorecard";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+
+    //Content provider Paths
     public static final String PATH_GOLF_FIELD = "golf_field";
+    public static final String PATH_GOLF_FIELD_FAVORITE = "golf_field_favorite";
+
     public static final String PATH_GOLF_FIELD_HOLE = "golf_field_hole";
+    public static final String PATH_GOLF_FIELD_HOLE_FIELD = "golf_field_hole-field";
+
     public static final String PATH_SCORECARD = "scorecard";
     public static final String PATH_SCORECARD_HOLE = "scorecard_hole";
 
@@ -28,28 +35,29 @@ public class ScorecardContract {
         public static final String TABLE_NAME = "golf_field";
 
         // Columns
-
-        //TODO add the columns
-        /*
-        public static final String COULUMN_MOVIE_TITLE = "mov_title";
-        public static final String COULUMN_MOVIE_IMAGE_THUMBNAIL = "mov_imageThumbnail";
-        public static final String COULUMN_MOVIE_SYSNOPSIS = "mov_mov_sysnopsis";
-        public static final String COULUMN_MOVIE_USER_RATING = "mov_userRating";
-        public static final String COULUMN_MOVIE_RELEASE_DATE = "mov_releaseDate";
-        */
+        public static final String COLUMN_GOLF_FIELD_NAME = "gf_name";
+        public static final String COLUMN_GOLF_FIELD_FAVORITE = "gf_favorite";
 
 
-
-        // Create content uri
+        // Create content uri for the golf fields
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
                 .appendPath(PATH_GOLF_FIELD)
                 .build();
 
-        // create cursor of base type directory for multiples entries
+        // Create content uri for the favorite golf fields
+        public static final Uri CONTENT_URI_FAVORITE = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_GOLF_FIELD_FAVORITE)
+                .build();
+
+        // create cursor of base type directory for multiples entries (Golf fields)
         public static final String CONTENT_DIR_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_GOLF_FIELD;
 
-        // create cursor of base type item for single entry
+        // create cursor of base type item for single entry (single golf field)
         public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_GOLF_FIELD;
+
+
+        // create cursor of base type directory for multiple entry (favorite golf fields)
+        public static final String CONTENT_DIR_TYPE_FAVORITE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_GOLF_FIELD_FAVORITE;
 
 
         //for building URIs on insertion
@@ -63,11 +71,70 @@ public class ScorecardContract {
         }
 
 
+        public static Uri buildAllGolfFieldsFavoriteUri() {
+            return CONTENT_URI_FAVORITE;
+        }
+
+
     }
 
 
 
     public static final class GolfFieldHoleEntry implements BaseColumns{
+
+
+        // Table name
+        public static final String TABLE_NAME = "golf_field_hole";
+
+        // Columns
+        public static final String COLUMN_GOLF_FIELD_HOLE_GF_ID = "gfH_gf_id";
+        public static final String COLUMN_GOLF_FIELD_FAVORITE = "gfH_number";
+        public static final String COLUMN_GOLF_FIELD_LENGTH = "gfH_length";
+        public static final String COLUMN_GOLF_FIELD_PAR = "gfH_par";
+
+
+        // Create content uri for the golf fields
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_GOLF_FIELD_HOLE)
+                .build();
+
+        // Create content uri for the golf fields
+        public static final Uri CONTENT_URI_FIELD = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_GOLF_FIELD_HOLE_FIELD)
+                .build();
+
+
+        // create cursor of base type directory for multiples entries (Golf fields holes)
+        public static final String CONTENT_DIR_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_GOLF_FIELD_HOLE;
+
+        // create cursor of base type item for single entry (single hole)
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_GOLF_FIELD_HOLE;
+
+
+        // create cursor of base type directory for multiple entry (holes for an especific fiueld)
+        public static final String CONTENT_DIR_TYPE_FIELD = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_GOLF_FIELD_HOLE_FIELD;
+
+
+
+        //for building URIs on insertion
+        public static Uri buildGolfFieldHoleUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+
+        public static Uri buildAllGolfFieldHoleUri() {
+            return CONTENT_URI;
+        }
+
+
+        public static Uri buildAllGolfFieldsHolesByFieldUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI_FIELD, id);
+        }
+
+
+
+
+
 
     }
 
